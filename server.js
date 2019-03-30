34cac10e0340d76f0f9437ff416bad82c5de135c
Nathan Swift <nswiftnswift@gmail.com>
2019-03-29 22:38:38 -0600

    initialized and full build

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+   instanceLocator: 'v1:us1:fb74fb44-5f53-47a8-8dac-86062de9cd2d',
+    key: 'd8859188-7693-4f02-8c70-3f6ca3c3b31a:WnhXNHfps57ZM7EmyRPOVWR72YUBttWgMZhJJVpluf4=',
+  })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+    const { username } = req.body
+    chatkit
+      .createUser({
+        id: username,
+        name: username
+      })
+      .then(() => res.sendStatus(201))
+      .catch(error => {
+        if (error.error === 'services/chatkit/user_already_exists') {
+          res.sendStatus(200)
+        } else {
+          res.status(error.status).json(error)
+        }
+      })
+  })
+
+app.post('/authenticate', (req, res) => {
+    const authData = chatkit.authenticate({ userId: req.query.user_id })
+    res.status(authData.status).send(authData.body)
+  })
 
 const PORT = 3001
 app.listen(PORT, err => {
